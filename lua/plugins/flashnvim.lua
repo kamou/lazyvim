26999eb5072e4f922bd03891ac1b2c6ee49eda1c
Ayman Khamouma <akhamouma@prophesee.ai>
2024-10-18 02:02:38 +0200

    Disable default mappings for flashnvim

---
+++ lua/plugins/flashnvim.lua
@@ -0,0 +1,10 @@
+-- disable default mappings for all modes
+return {
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+    },
+  },
+}
